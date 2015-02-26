fdf0dc503a1df85dba1f8446dbc052a26a728444
PalDeshpande <pallavi.pkeskar@gmail.com>
2015-02-26 11:10:24 -0800

    Second commit

--- WordFrequencyTests/WordFrequencyTests.m
+++ WordFrequencyTests/WordFrequencyTests.m
@@ -10,6 +10,8 @@
 #import <XCTest/XCTest.h>
 
 @interface WordFrequencyTests : XCTestCase
+@property (nonatomic)ViewController *homeVC;
+@property (nonatomic)WordsByFrequencyTableViewController *wordsVC;
 
 @end
 
@@ -17,7 +19,9 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.homeVC = [[ViewController alloc] init];
+    self.wordsVC = [[WordsByFrequencyTableViewController alloc] init];
 }
 
 - (void)tearDown {
@@ -29,6 +33,7 @@
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
+
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
